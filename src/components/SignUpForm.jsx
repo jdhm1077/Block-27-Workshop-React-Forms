74d8f300aa732b15bb3be9653596603f8668035f
Jesus Hernandez <jesushernandez@Jesuss-MacBook-Pro-2.local>
2023-11-17 16:43:05 -0500

    setPassword(event.target.value) writing on form

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -11,18 +11,42 @@ const SignUpForm = () => {
    //define an async function called "handleSubmit"
    const handleSubmit = async (event) => {
     event.preventDefault(); // Prevent page refresh
-    console.log('Form submitted!');
-   }
+    //console.log('Form submitted!');
+        try {
+            const response = await fetch ("https://fsa-jwt-practice.herokuapp.com/signup",
+                {method: "POST",
+            headers: {
+                "Content-Type" : "aplicaction/json"
+            },
+            body: JSON.stringify ({
+                username: username,
+                password: password
+            })
+        });
+
+        const result = await response.json();
+        console.log(result);
+                
+
+
+            setError(null);
+        } catch (error) {
+            setError(error.message);
+        }
+   };
 
     return (
     <>
-    <h2>Sign Up!</h2>
+     <h2>Sign Up!</h2>
+
+         {error && <p>{error}</p>}
+
             <form onSubmit={handleSubmit}>
                 <label>
-                Username: <input value={username} onChange={(event) => (event. target. value)} />
+                Username: <input type="text" value={username} onChange={(event) => (setUsername (event.target.value) )} />
                 </label>
                 <label>
-                Password: <input value={password} onChange={(event) => (event. target. value)}/>
+                Password: <input type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
                 </label>
                 <button>Submit</button>
             </form>
